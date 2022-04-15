4711f5979b54c2a1c2b1ef69d00042999ba9d871
Kristijan Postolov <Kristijan.Postolov@netcetera.com>
2022-04-15 20:32:47 +0200

    Spring server module setup

---
+++ iot-server/src/main/java/com/kristijan/iotdesk/server/IotServerApplication.java
@@ -0,0 +1,12 @@
+package com.kristijan.iotdesk.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IotServerApplication.class, args);
+  }
+}
---
+++ iot-server/src/test/java/com/kristijan/iotdesk/server/IotServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kristijan.iotdesk.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotServerApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
