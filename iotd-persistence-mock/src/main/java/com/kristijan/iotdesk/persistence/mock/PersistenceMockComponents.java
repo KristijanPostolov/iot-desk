985cac0570a8c4e8eeb1ff06f80a8833b68d0c31
Kristijan Postolov <Kristijan.Postolov@netcetera.com>
2022-09-05 23:03:17 +0200

    Delete iotd-persistence-mock module

--- iotd-persistence-mock/src/main/java/com/kristijan/iotdesk/persistence/mock/PersistenceMockComponents.java
+++
@@ -1,7 +0,0 @@
-package com.kristijan.iotdesk.persistence.mock;
-
-/**
- * Marker interface for spring boot scanning of mock persistence implementation.
- */
-public interface PersistenceMockComponents {
-}
