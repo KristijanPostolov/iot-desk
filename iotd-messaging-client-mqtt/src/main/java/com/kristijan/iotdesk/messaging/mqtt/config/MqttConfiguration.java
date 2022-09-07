fc73a2474cc3db9b9f92ccc13fcf0446d9d4b81e
Kristijan Postolov <Kristijan.Postolov@netcetera.com>
2022-09-08 00:02:02 +0200

    Use mqtt memory persistence for tests

--- iotd-messaging-client-mqtt/src/main/java/com/kristijan/iotdesk/messaging/mqtt/config/MqttConfiguration.java
+++ iotd-messaging-client-mqtt/src/main/java/com/kristijan/iotdesk/messaging/mqtt/config/MqttConfiguration.java
@@ -2,8 +2,11 @@ package com.kristijan.iotdesk.messaging.mqtt.config;
 
 import com.kristijan.iotdesk.messaging.mqtt.services.MqttMessageHandler;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,13 +24,17 @@ public class MqttConfiguration {
   private String clientId;
   @Value("${mqtt.clean-session:false}")
   private boolean cleanSession;
+  @Value("${mqtt.with-disk-client-persistence:false}")
+  private boolean withDiskClientPersistence;
 
   @Bean
   public MqttClient mqttClient(Set<MqttMessageHandler> handlers) {
     try {
       MqttConnectOptions connectOptions = new MqttConnectOptions();
       connectOptions.setCleanSession(cleanSession);
-      MqttClient mqttClient = new MqttClient(brokerUrl, clientId);
+      MqttClientPersistence clientPersistence =
+        withDiskClientPersistence ? new MqttDefaultFilePersistence() : new MemoryPersistence();
+      MqttClient mqttClient = new MqttClient(brokerUrl, clientId, clientPersistence);
       mqttClient.connect(connectOptions);
 
       handlers.forEach(handler -> subscribeHandler(mqttClient, handler));
