75b6932345d9d7d0b9066a3926a556061aaca626
Kristijan Postolov <Kristijan.Postolov@netcetera.com>
2022-09-08 09:55:56 +0200

    memory persistence

--- iotd-messaging-client-mqtt/src/main/java/com/kristijan/iotdesk/messaging/mqtt/config/MqttConfiguration.java
+++ iotd-messaging-client-mqtt/src/main/java/com/kristijan/iotdesk/messaging/mqtt/config/MqttConfiguration.java
@@ -4,6 +4,7 @@ import com.kristijan.iotdesk.messaging.mqtt.services.MqttMessageHandler;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +28,7 @@ public class MqttConfiguration {
     try {
       MqttConnectOptions connectOptions = new MqttConnectOptions();
       connectOptions.setCleanSession(cleanSession);
-      MqttClient mqttClient = new MqttClient(brokerUrl, clientId, null);
+      MqttClient mqttClient = new MqttClient(brokerUrl, clientId, new MemoryPersistence());
       mqttClient.connect(connectOptions);
 
       handlers.forEach(handler -> subscribeHandler(mqttClient, handler));
